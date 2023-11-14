59fb0a45df22c5aa76dff712da6796d21934cc8b
Eric Fried <ebfried@me.com>
2023-11-14 23:02:25 +0000

    added input event listener to photo URL input that sets src of photo preview

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 // /* global data */
+const $photoURL = document.querySelector('#URL');
+const $img = document.querySelector('#preview');
+
+$photoURL.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  $img.setAttribute('src', event.target.value);
+  console.log($img.getAttribute('src'));
+}
+
+const $entryForm = document.querySelector('#entry-form');
+console.dir($entryForm);
+console.log($entryForm.elements);
+$entryForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log($entryForm.elements);
+
+  const entryFormValues = {
+    title: $entryForm.title.value,
+    URL: $entryForm.URL.value,
+    Notes: $entryForm.Notes.value,
+  };
+
+  console.log(entryFormValues);
+}
