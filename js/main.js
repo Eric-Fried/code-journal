ba61979571bfa65f3cbfc8020855279dd803870a
Eric Fried <ebfried@me.com>
2023-11-16 18:54:36 +0000

    defined renderEntry function to create DOM tree properly

--- js/main.js
+++ js/main.js
@@ -26,3 +26,25 @@ function handleSubmit(event) {
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
 }
+
+// function renderEntry (entry) {
+
+//   const $li =document.createElement("li");
+//   $li.setAttribute('class', 'row');
+
+//   const $imgWrapper =document.createElement('div')
+//   $imgWrapper.setAttribute('class', 'column-half')
+
+//   const $img = document.createElement('img')
+//   $img.setAttribute ('src', entry.url)
+
+//   const $contentWrapper =document.createElement('div')
+//   $contentWrapper.setAttribute('class', 'column-half')
+
+//   const $title =document.createElement('h3')
+//   $title.textContent =entry.title
+
+//   const $notes = document.createElement('p')
+//   $notes.textContent= entry.notes
+
+// }
